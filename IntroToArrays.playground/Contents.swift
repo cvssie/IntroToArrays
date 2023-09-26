e5f4466e9ae086ca78c3905ee719799a268713b5
Cassandra K <ckk30@hawaii.edu>
2023-09-26 13:25:20 -1000

    completed steps 1-9
    learning the basics of an array and how to declare a type

--- IntroToArrays.playground/Contents.swift
+++ IntroToArrays.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var greeting = "Hello, playground"
+//pt 3 declare an array named fruitNames and type string
+var fruitNames: [String]
+
+//PART 4
+fruitNames = ["Apple", "Orange", "Pears"]
+
+//PART 5 ON YOUR OWN
+print(fruitNames)
+
+//part 6
+print("I like to eat \(fruitNames)")
+
+//part 7
+for name in fruitNames
+{
+  //part 8
+    print("I like to eat " + name)
+
+}
+//part 9
+print(fruitNames[2])
+
